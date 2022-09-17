adc88964f6d374d91b6803fb90a639ea58baefcc
Artem Kiyashko <artem86@hotmail.co.uk>
2022-09-17 20:00:41 +0200

    add reply markup to announcements

--- AnnouncementProcessor/Proxies/TelegramBotMessageProxy.cs
+++ AnnouncementProcessor/Proxies/TelegramBotMessageProxy.cs
@@ -18,11 +18,11 @@ namespace AnnouncementProcessor.Proxies
 
         public async Task<Message> SendAnnouncement(AnnouncementMessage announcement) => announcement.MessageType switch
         {
-            MessageType.Text => await _telegramBotClient.SendTextMessageAsync(announcement.ChatId, text: announcement.Text),
-            MessageType.Photo => await _telegramBotClient.SendPhotoAsync(announcement.ChatId, photo: announcement.ContentUrl.AbsoluteUri, caption: announcement.Text),
-            MessageType.Video => await _telegramBotClient.SendVideoAsync(announcement.ChatId, video: announcement.ContentUrl.AbsoluteUri, caption: announcement.Text),
-            MessageType.VideoNote => await _telegramBotClient.SendVideoNoteAsync(announcement.ChatId, videoNote: announcement.ContentUrl.AbsoluteUri),
-            MessageType.Voice => await _telegramBotClient.SendVoiceAsync(announcement.ChatId, voice: announcement.ContentUrl.AbsoluteUri),
+            MessageType.Text => await _telegramBotClient.SendTextMessageAsync(announcement.ChatId, text: announcement.Text, replyMarkup: announcement.ReplyMarkup),
+            MessageType.Photo => await _telegramBotClient.SendPhotoAsync(announcement.ChatId, photo: announcement.ContentUrl.AbsoluteUri, caption: announcement.Text, replyMarkup: announcement.ReplyMarkup),
+            MessageType.Video => await _telegramBotClient.SendVideoAsync(announcement.ChatId, video: announcement.ContentUrl.AbsoluteUri, caption: announcement.Text, replyMarkup: announcement.ReplyMarkup),
+            MessageType.VideoNote => await _telegramBotClient.SendVideoNoteAsync(announcement.ChatId, videoNote: announcement.ContentUrl.AbsoluteUri, replyMarkup: announcement.ReplyMarkup),
+            MessageType.Voice => await _telegramBotClient.SendVoiceAsync(announcement.ChatId, voice: announcement.ContentUrl.AbsoluteUri, replyMarkup: announcement.ReplyMarkup),
             _ => null
         };
     }
