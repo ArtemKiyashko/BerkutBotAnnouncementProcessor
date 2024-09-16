aca5ce2bd95fcb916da411420526630d09d39af7
Artem Kiyashko <artem86@hotmail.co.uk>
2024-09-16 13:05:18 +0200

    Support bigger videos

--- AnnouncementProcessor/Proxies/TelegramBotMessageProxy.cs
+++ AnnouncementProcessor/Proxies/TelegramBotMessageProxy.cs
@@ -25,7 +25,7 @@ namespace AnnouncementProcessor.Proxies
         {
             MessageType.Text => await _telegramBotClient.SendTextMessageAsync(announcement.ChatId, text: announcement.Text, replyMarkup: announcement.ReplyMarkup),
             MessageType.Photo => await _telegramBotClient.SendPhotoAsync(announcement.ChatId, photo: InputFile.FromUri(announcement.ContentUrl), caption: announcement.Text, replyMarkup: announcement.ReplyMarkup),
-            MessageType.Video => await _telegramBotClient.SendVideoAsync(announcement.ChatId, video: InputFile.FromUri(announcement.ContentUrl), caption: announcement.Text, replyMarkup: announcement.ReplyMarkup),
+            MessageType.Video => await _telegramBotClient.SendVideoAsync(announcement.ChatId, video: InputFile.FromStream(await _blobServiceManager.GetContentAsStream(announcement.ContentUrl)), caption: announcement.Text, replyMarkup: announcement.ReplyMarkup, height: announcement.ContentProperties.Height, width: announcement.ContentProperties.Width, supportsStreaming: announcement.ContentProperties.SupportStreaming),
             MessageType.VideoNote => await _telegramBotClient.SendVideoNoteAsync(announcement.ChatId, videoNote: InputFile.FromUri(announcement.ContentUrl), replyMarkup: announcement.ReplyMarkup),
             MessageType.Voice => await _telegramBotClient.SendVoiceAsync(announcement.ChatId, voice: InputFile.FromStream(await _blobServiceManager.GetContentAsStream(announcement.ContentUrl)), caption: announcement.Text, replyMarkup: announcement.ReplyMarkup),
             MessageType.Audio => await _telegramBotClient.SendAudioAsync(announcement.ChatId, audio: InputFile.FromUri(announcement.ContentUrl), caption: announcement.Text),
---
+++ AnnouncementProcessor/ViewModels/ContentProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace AnnouncementProcessor.ViewModels
+{
+	public class ContentProperties
+	{
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+        public bool? SupportStreaming { get; set; }
+    }
+}
+
